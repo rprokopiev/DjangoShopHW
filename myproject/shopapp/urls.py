9727e43a0fb5f8fc21b1e1b4d33fcc23c34ca9ab
Renat Prokopiev <prokop4891@gmail.com>
2024-02-20 17:22:48 +0300

    HW3, templates

--- myproject/shopapp/urls.py
+++ myproject/shopapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('main/', views.main, name='main'),
     path('about/', views.about, name='about'),
+    path('client_orders/<str:phone>', views.client_orders, name='client_orders'),
 ]
