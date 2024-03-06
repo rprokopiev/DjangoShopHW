8883c9863ee011ed8a54185431d4e90cae8fe431
Renat Prokopiev <prokop4891@gmail.com>
2024-03-06 21:08:56 +0300

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 SECRET_KEY = os.getenv['SECRET_KEY']
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SESSION_COOKIE_SECURE = True
